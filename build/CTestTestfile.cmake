32f24311a3ed61860cfd31968bce67b6999ebeb7
Bo Lan bl2947 <113029391+RamboLan1@users.noreply.github.com>
2023-12-29 22:02:36 -0500

    Add files via upload

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,30 @@
+# CMake generated Testfile for 
+# Source directory: /home/rambo/ros_motion_planning/src
+# Build directory: /home/rambo/ros_motion_planning/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("third_party/dynamic_xml_config")
+subdirs("third_party/dynamic_rviz_config")
+subdirs("third_party/rviz_plugins/spencer_messages/spencer_control_msgs")
+subdirs("third_party/rviz_plugins/spencer_messages/spencer_human_attribute_msgs")
+subdirs("third_party/rviz_plugins/spencer_messages/spencer_tracking_msgs")
+subdirs("third_party/gazebo_plugins/pedestrian_visualizer_plugin/pedsim_msgs")
+subdirs("third_party/rviz_plugins/spencer_messages/spencer_social_relation_msgs")
+subdirs("third_party/rviz_plugins/spencer_messages/spencer_vision_msgs")
+subdirs("third_party/gazebo_plugins/pedestrian_sfm_plugin")
+subdirs("third_party/gazebo_plugins/pedestrian_visualizer_plugin/gazebo_ped_visualizer_plugin")
+subdirs("sim_env")
+subdirs("third_party/rviz_plugins/spencer_tracking_rviz_plugin")
+subdirs("planner/utils")
+subdirs("planner/global_planner/global_planner")
+subdirs("planner/local_planner/local_planner")
+subdirs("planner/local_planner/dwa_planner")
+subdirs("planner/local_planner/apf_planner")
+subdirs("planner/global_planner/evolutionary_planner")
+subdirs("planner/local_planner/pid_planner")
+subdirs("planner/global_planner/sample_planner")
+subdirs("planner/local_planner/static_planner")
+subdirs("third_party/map_plugins/voronoi_layer")
+subdirs("planner/global_planner/graph_planner")
